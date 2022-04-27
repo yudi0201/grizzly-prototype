e587eb431ed4650dd276d6fb955dcaccb42138ca
yudi0201 <yudi.sun19@gmail.com>
2022-04-26 22:50:34 -0400

    initial changes

---
+++ build/CMakeFiles/grizzly-lib.dir/DependInfo.cmake
@@ -0,0 +1,55 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/yudi/Code/grizzly-prototype/src/api/Aggregation.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/api/Aggregation.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/api/Assigner.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/api/Assigner.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/api/Config.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/api/Config.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/api/Field.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/api/Field.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/api/Predicate.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/api/Predicate.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/api/Query.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/api/Query.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/api/Schema.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/api/Schema.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/api/Time.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/api/Time.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/api/Trigger.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/api/Trigger.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/code_generation/CCode.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/code_generation/CCode.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/code_generation/CFile.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/code_generation/CFile.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/code_generation/CMethod.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/code_generation/CMethod.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/code_generation/CodeGenerator.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/code_generation/CodeGenerator.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/code_generation/QueryContext.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/code_generation/QueryContext.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/jit/CodeCompiler.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/jit/CodeCompiler.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/jit/JITCodeGenerator.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/jit/JITCodeGenerator.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/jit/JITExecutionRuntime.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/jit/JITExecutionRuntime.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/jit/Profiling.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/jit/Profiling.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/jit/Variant.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/jit/Variant.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/operator/AggregateOperator.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/operator/AggregateOperator.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/operator/FilterOperator.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/operator/FilterOperator.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/operator/FinalWindowAggOperator.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/operator/FinalWindowAggOperator.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/operator/GenerateOperator.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/operator/GenerateOperator.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/operator/GroupByOperator.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/operator/GroupByOperator.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/operator/InputOperator.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/operator/InputOperator.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/operator/KeyOperator.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/operator/KeyOperator.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/operator/MapOperator.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/operator/MapOperator.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/operator/Operator.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/operator/Operator.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/operator/PrintOperator.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/operator/PrintOperator.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/operator/ReadOperator.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/operator/ReadOperator.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/operator/ReadWindowOperator.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/operator/ReadWindowOperator.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/operator/SelectOperator.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/operator/SelectOperator.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/operator/WindowOperator.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/operator/WindowOperator.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/operator/WriteOperator.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/operator/WriteOperator.cpp.o"
+  "/home/yudi/Code/grizzly-prototype/src/operator/WriteToMemOperator.cpp" "/home/yudi/Code/grizzly-prototype/build/CMakeFiles/grizzly-lib.dir/src/operator/WriteToMemOperator.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/grizzly-lib.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libgrizzly-lib.a"
+)
